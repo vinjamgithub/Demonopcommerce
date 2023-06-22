2c6a51efd2b10c72990dca53a011aef88190474b
vinjamgithub <vinjamvenky79@gmail.com>
2023-06-22 23:28:16 +0530

    updated

--- run.bat
+++ run.bat
@@ -1,5 +1,2 @@
 pytest -s -v -m "sanity and regression" --html=Reports\report.html testcases/ --browser chrome
 
-rem pytest -s -v -m "sanity or regression" --html=Reports\report.html testcases/ --browser chrome
-
-rem pytest -s -v -m "sanity" --html=Reports\report.html testcases/ --browser chrome
